592b61286594842b4c84b510d04db08db22ffaa4
Drew Harris <harrisd@smu.edu>
2023-10-12 11:48:46 -0500

    Add calculations

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
 #include "./lib/calc.h"
+#include <iostream>
+
+int main() {
+    Calculator calc;
+    std::cout << "Hello world" << std::endl;
+    std::cout << "1 + 2 = " << calc.Add(1, 2) << std::endl;
+    std::cout << "1 - 2 = " << calc.Sub(1, 2) << std::endl;
+    std::cout << "1 * 2 = " << calc.Mul(1, 2) << std::endl;
+    std::cout << "1 / 2 = " << calc.Div(1, 2) << std::endl;
+    return 0;
+}
